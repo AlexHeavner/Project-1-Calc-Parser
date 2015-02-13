892a407da7fbefb744a2dd38ef240bcfc4f84627
brentzucker <brentzucker@gmail.com>
2015-02-12 19:49:44 -0500

    Testing parser in test.java
    The valid program comes back invalid…

--- Test.java
+++ Test.java
@@ -1,14 +1,25 @@
 import java.util.LinkedList;
 
 
-public class Test {
+public class Test 
+{
 	public static void main(String[] args)
 	{
 		CalcScanner scanner = new CalcScanner("myprog.calc");
 		
-		LinkedList<Token> prog = scanner.scan();
+		LinkedList<Token> token_list = scanner.scan();
 		
-		for(Token element: prog)
+		for(Token element: token_list)
 			System.out.println(element.getToken() + " "+element.getType() + " " +element.getLine());
+		
+		System.out.println("Scanning Complete, beginning to Parse.");
+
+		CalcParser parser = new CalcParser(token_list);
+
+		if(parser.parse())
+			System.out.println("The program is valid.");
+		else 
+			System.out.println("The program is invalid");
+
 	}
 }
