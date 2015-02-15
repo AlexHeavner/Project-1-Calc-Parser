6be7f1e61cb0d3dd2568234d1a20f021c314387e
AlexHeavner <alexander.heavner@bobcats.gcsu.edu>
2015-02-15 13:47:00 -0500

    CalcWriter writes a feasible program

--- CalcWriter.java
+++ CalcWriter.java
@@ -23,12 +23,34 @@ public class CalcWriter {
 			writer.write("import java.util.Scanner; \n public class " + file_name + "{ \n " +
 					"public static void main(String[] args){ \n" +
 					"Scanner scan = new Scanner(System.in);\n");
-			
-			for(LinkedList<Token> list_element: statement_list)
+			Token prev_element = null;
+			String helper = "";
+			for(LinkedList<Token> element: statement_list)
 			{
-				for(Token element: list_element)
+				if(element.get(0).getType() == Type.READ)
 				{
-
+					writer.write("double " + element.get(1).getToken() + ";\n" +
+							element.get(1).getToken() + "= scan.nextDouble();\n");
+				}
+				
+				else if(element.get(0).getType() == Type.WRITE)
+				{
+					writer.write("System.out.println(");
+					
+					for(int count = 1 ; count < element.size(); count++)
+						writer.write(element.get(count).getToken());
+					
+					writer.write(");");
+				}
+				else if(element.get(0).getType() == Type.ID)
+				{
+					writer.write("double "+ element.get(0).getToken() + ";");
+					writer.write(element.get(0).getToken() + "=");
+					
+					for(int count = 2 ; count < element.size(); count++)
+						writer.write(element.get(count).getToken());
+					
+					writer.write(";");
 				}
 			}
 			
